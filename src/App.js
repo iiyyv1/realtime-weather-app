f8fd482850d52f9e286266e7636141368235da72
iiyy <iiyyv1@gmail.com>
2021-10-28 22:57:57 +0800

    finish Realtime Weather App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo } from 'react';
+import styled from '@emotion/styled';
+import { ThemeProvider } from '@emotion/react';  // update
 
-function App() {
+import WeatherCard from './views/WeatherCard';
+import WeatherSetting from './views/WeatherSetting';
+
+import useWeatherAPI from './hooks/useWeatherAPI';
+
+import { getMoment, findLocation } from './utils/helpers';
+
+
+const theme = {
+  light: {
+    backgroundColor: '#ededed',
+    foregroundColor: '#f9f9f9',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#212121',
+    temperatureColor: '#757575',
+    textColor: '#828282',
+  },
+  dark: {
+    backgroundColor: '#1F2022',
+    foregroundColor: '#121416',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)',
+    titleColor: '#f9f9fa',
+    temperatureColor: '#dddddd',
+    textColor: '#cccccc',
+  },
+};
+
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.backgroundColor};
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AUTHORIZATION_KEY = 'CWB-7A858508-F984-4BBB-8D18-852DBA49B5E2';
+// const LOCATION_NAME = '臺北';
+// const LOCATION_NAME_FORECAST = '臺北市';
+
+
+const App = () => {
+  const storageCity = localStorage.getItem('cityName') || '臺北市';
+
+  const [currentCity, setCurrentCity] = useState(storageCity);
+  const currentLocation = useMemo(()=>findLocation(currentCity), [currentCity]);
+  const { cityName, locationName, sunriseCityName } = currentLocation;
+
+  const [weatherElement, fetchData] = useWeatherAPI({
+    locationName,
+    cityName,
+    authorizationKey: AUTHORIZATION_KEY,
+  });
+
+  const[currentPage, setCurrentPage] = useState('WeatherCard');
+  const handleCurrentPageChange = (currentPage) => {
+    setCurrentPage(currentPage);
+  };
+
+  const handleCurrentCityChange = (currentCity) => {
+    setCurrentCity(currentCity);
+  };
+
+  const [currentTheme, setCurrentTheme] = useState('light');
+
+  const moment = useMemo(() => getMoment(sunriseCityName), [sunriseCityName]);
+
+  useEffect(() => {
+    setCurrentTheme(moment === 'day' ? 'light' : 'dark');
+  }, [moment]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme[currentTheme]}>
+      <Container>
+        {currentPage === 'WeatherCard' && (
+          <WeatherCard
+            cityName={cityName}
+            weatherElement={weatherElement}
+            moment={moment}
+            fetchData={fetchData}
+            handleCurrentPageChange={handleCurrentPageChange}
+          />
+        )}
+      
+      {currentPage === 'WeatherSetting' && (
+        <WeatherSetting
+          cityName={cityName}
+          handleCurrentCityChange={handleCurrentCityChange}
+          handleCurrentPageChange={handleCurrentPageChange} 
+        />
+      )}
+      </Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
